91a24ec60dae52d64b0b1d24a18f25d08283b6be
Duncan Fraiser <duncan.fraiser@ergon.com>
2017-02-22 11:16:33 -0600

    origon

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+ 
+//Default Welcome Routes and Auth Routes
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//Auth::routes();
+
+// Route::get('/', 'HomeController@index');
+
+
+
+// My Routes
+Auth::routes();
+Route::resource('/', 'HomeController');
